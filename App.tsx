635a625e98fa12b527f3b49f5cd5546e562454f2
azarGIT171987 <32077385+azarGIT171987@users.noreply.github.com>
2022-10-07 12:07:22 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  let intitalState = 0;
+  const [timer, setTimer] = React.useState(intitalState);
+  const [active, setActive] = React.useState(false);
+
+  function toggle() {
+    setActive(!active);
+  }
+
+  function resetTimer() {
+    setTimer(0);
+    setActive(false);
+  }
+
+  React.useEffect(() => {
+    let interval = null;
+    if (active) {
+      interval = setInterval(() => {
+        setTimer((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!active && timer !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [active, timer]);
+
+  return (
+    <div>
+      <h1>{timer}</h1>
+      <button onClick={toggle}>{active ? 'Pause' : 'Start'}</button>
+      <button onClick={resetTimer}>Reset</button>
+      <p>Start or Pause the Timer</p>
+    </div>
+  );
+}
